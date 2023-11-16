72dcdaf95a50ce4cfc01198214e2dffe5910a058
jungle <junglecatsw@gmail.com>
2023-11-16 12:58:19 +0000

    client gen

---
+++ codegen.sh
@@ -0,0 +1,11 @@
+rm -rf src
+rm -rf swagger-codegen
+git clone https://github.com/swagger-api/swagger-codegen
+cp swagger/public.swagger.json swagger-codegen
+cd swagger-codegen
+./run-in-docker.sh mvn package
+./run-in-docker.sh generate -i public.swagger.json \
+    -l python -o /gen/out -DpackageName=src.cudo_compute
+cd ..
+cp -r swagger-codegen/out/* .
+
