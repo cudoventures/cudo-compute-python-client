67ce6c974e5223c65753bb2ed17972267aecb0f7
jungle <junglecatsw@gmail.com>
2023-11-23 13:49:18 +0000

    fixes

--- exp/example.py
+++ exp/example.py
@@ -1,14 +1,29 @@
 import cudo_compute as cudo
 def machine_types(gpu_model, mem_gib, vcpu_count, gpu_count):
     try:
-        c, e = cudo.CudoClient.get_client()
-        api = cudo.VirtualMachinesApi(c)
+        client, e = cudo.CudoClient.get_client()
+        api = cudo.VirtualMachinesApi(client)
         types = api.list_vm_machine_types(mem_gib, vcpu_count, gpu=gpu_count, gpu_model=gpu_model)
         types_dict = types.to_dict()
         return types_dict
-    except Exception as e:  # TODO what to do with errors ?
+    except Exception as e:
         raise e
 
 
-t = machine_types("",4,4,0)
-print(t)
+print(machine_types("",4,4,0))
+
+
+def list_instances():
+    try:
+        project_id, e = cudo.AuthConfig.get_project()
+        client, e = cudo.CudoClient.get_client()
+        api = cudo.VirtualMachinesApi(client)
+        vms = api.list_vms(project_id)
+        instances = {}
+        vms_dict = vms.to_dict()
+        return vms_dict
+    except Exception as e:
+        raise e
+
+print(list_instances())
+
